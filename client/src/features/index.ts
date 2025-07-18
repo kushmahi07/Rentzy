117973011f450e0e15cd626815ecc1e0c1edf01d
kushmahi07 <kushagra.ranjan217@gmail.com>
2025-07-18 16:01:32 +0530

    Add files via upload

---
+++ client/src/features/index.ts
@@ -0,0 +1,12 @@
+// Export all feature modules
+export * from './auth';
+export * from './dashboard';
+export * from './user-management';
+export * from './booking-management';
+export * from './property-management';
+export * from './role-management';
+export * from './settings';
+export * from './profile';
+export * from './tokenization';
+export * from './investment-oversight';
+export * from './secondary-marketplace';
